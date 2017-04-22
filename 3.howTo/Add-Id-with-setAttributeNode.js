80f5070474851c31e2364cbce0a0a92fb9f3fc2e
SL <stefan22@users.noreply.github.com>
2017-04-22 22:11:45 +0100

    Update Add-Id-with-setAttributeNode.js

--- 3.howTo/Add-Id-with-setAttributeNode.js
+++ 3.howTo/Add-Id-with-setAttributeNode.js
@@ -1,1 +1,21 @@
+/*
+How to add an Id through setAttributeNode 
+
+*/  
+
+//create an id
+var id = document.getElementById('id');
+=>id=''
+
+//set value
+id.value = 'myId';
+
+//create a div
+var div = document.createElement('div');
+
+//append attribute
+div.setAttributeNode(id);
+
+div
+=> <div id='myId'></div>
 
