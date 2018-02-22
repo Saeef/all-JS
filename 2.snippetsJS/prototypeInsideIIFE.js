a8da30fb8767c31684de6ea7a0e549154fd2a5f0
Stefano L <stefan22@users.noreply.github.com>
2018-02-22 21:41:26 +0000

    Create prototypeInsideIIFE.js

---
+++ 2.snippetsJS/prototypeInsideIIFE.js
@@ -0,0 +1,61 @@
+(function(f, undefined) {
+    
+
+    /*
+    
+      note to self:
+      
+      accessing a prototype method from inside of an IIFE
+    
+    
+    
+    */
+    
+
+
+    var Person = function(first,last,age) {
+        this.first = first;
+        this.last = last;
+        this.age = age;
+
+    };
+
+    Person.prototype.getFullName = function(){
+        var isthis = this;
+        var nested = function() {
+            var fnname = isthis.first + " " + isthis.last;
+            console.log(fname);
+        
+        };//nested
+
+        return nested();
+
+    };//getFullName prototype
+
+
+
+    var person = new Person('Angel', 'Di Maria', 29);
+
+    console.dir(person);
+
+
+    /*
+    
+      best way no need to set public fns
+      var person = new f.Person('Angel','Di Maria',29);
+      
+      person.getFullName();
+      
+    */
+
+
+
+    return f;
+
+
+
+
+
+
+
+})(window.f = window.f || {});
